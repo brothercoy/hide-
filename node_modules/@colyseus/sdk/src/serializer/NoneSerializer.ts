3213502429ca6f9dd404eff3c593ccc002aab22e
Brothercoy <chriz0204@gmail.com>
2026-05-07 18:21:19 -0400

    remove node_modules, add gitignore

--- node_modules/@colyseus/sdk/src/serializer/NoneSerializer.ts
+++
@@ -1,9 +0,0 @@
-import type { Serializer } from "./Serializer.ts";
-
-export class NoneSerializer<T = any> implements Serializer<T> {
-    setState(rawState: any): void {}
-    getState() { return null; }
-    patch(patches) {}
-    teardown() { }
-    handshake(bytes: Uint8Array) {}
-}
